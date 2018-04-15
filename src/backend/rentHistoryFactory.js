6c96437f5bf94a232a510431a0f82ec962ae64ba
Quinn <qck@case.edu>
2018-04-15 15:01:07 -0400

    Fixed Rent History

--- src/backend/rentHistoryFactory.js
+++ src/backend/rentHistoryFactory.js
@@ -18,58 +18,60 @@ module.exports = {
      });
   },
 
-  createRequest: function(propertyId, creatorId, request) {
+  createEntry: function(propertyId, creatorId, request) {
     database.open();
     const created = Date.now()
     request.status = module.exports.convertStatusToInt(request)
-    return database.query(`INSERT INTO maint_request
-      (id, property_id, creator_id, created_date, title, description, attached_files, worker_id, status)
-      VALUES(null,?,?,?,?,?,?,null,?);`,
+    return database.query(`INSERT INTO rent_history
+      (id, payer_id, property_id, payment_date, payment_amount, on_time)
+      VALUES (null, ?, ?, ?, ?, ?);`,
       [propertyId, creatorId, created, request.title, request.description, request.attachedFiles, request.worker_id, 1]).then( () => {
       return database.close();
     });
   },
 
-  deleteRequest: function(id) {
+  deleteEntry: function(id) {
     database.open();
-    return database.query(`DELETE FROM maint_request WHERE id = ?;`,
+    return database.query(`DELETE FROM rent_history WHERE id = ?;`,
                           [id]).then(() => {
       //Do I need to return results here?  Or does promise cover failure case
       return database.close();
     });
   },
 
-  editMaintRequest: function(request) {
+  editEntry: function(entry) {
     database.open();
-    return database.query(`UPDATE maint_request SET
-      title = ?, description = ?, attached_files = ?, worker_id = ?, status = ? WHERE id = ?;`,
-      [request.title, request.description, request.attachedFiles, 1, ]).then( () => {
+    return database.query(`UPDATE rent_history SET
+      payer_id = ?, property_id = ?, payment_amount = ?, on_time = ?
+      WHERE id = ?;`,
+      [entry.payerId, entry.propertyId, entry.paymentAmount, entry.onTime, entry.id]).then( () => {
       return database.close();
     });
   },
 
-  getCommentsByRequestId: function(requestId) {
-    let comments;
+  getEntriesForUser: function(userId) {
+    let entries;
       database.open()
-      return database.query('select * from comment where request_id = ?;', [requestId]).then( rows => {
-        comments = rows;
+      return database.query(`select * from rent_history where payer_id = ?;`, [userId]).then( rows => {
+        entries = rows;
         return database.close()
       } )
       .then( () => {
-      return comments;
+      return entries;
      });
   },
 
-  addCommentForRequest: function(requestId, creatorId, comment) {
-    database.open();
-    const created = Date.now()
-    return database.query(`insert into comment (id, request_id, creator_id, created_date, comment_text, attached_files)
-      VALUES (null, ?, ?, ?, ?, ?);`,
-      [requestId, creatorId, created, comment.text, comment.attachedFiles]).then( () => {
-      return database.close();
-    });
+  getEntriesForProperty: function(propertyId) {
+    let entries;
+      database.open()
+      return database.query(`select * from rent_history where property_id = ?;`, [propertyId]).then( rows => {
+        entries = rows;
+        return database.close()
+      })
+      .then( () => {
+      return entries;
+     });
   },
-
   //This lets us mock out the database, so that we can check calls, supply responses
   setDatabase: function(newDb) {
     database = newDb
