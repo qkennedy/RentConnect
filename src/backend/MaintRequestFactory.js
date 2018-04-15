c7c9d337fce039666b4837a86a562a04451c998c
Quinn <qck@case.edu>
2018-04-14 20:46:04 -0400

    Added Maintenance Request Factory

---
+++ src/backend/MaintRequestFactory.js
@@ -0,0 +1,85 @@
+var mysql = require('mysql');
+var Database = require('./database')
+const userFactory = require('./userFactory');
+const propertyFactory = require('./propertyFactory')
+
+var database = new Database();
+module.exports = {
+
+  getRequestById: function(id) {
+    let request;
+      database.open()
+      return database.query('select * from maint_request where id = ?;', [id]).then( rows => {
+        request = rows[0];
+        return database.close()
+      } )
+      .then( () => {
+      return request;
+     });
+  },
+
+  createRequest: function(propertyId, creatorId, request) {
+    database.open();
+    const created = Date.now()
+    request.status = module.exports.convertStatusToInt(request)
+    return database.query(`INSERT INTO maint_request
+      (id, property_id, creator_id, created_date, title, description, attached_files, worker_id, status)
+      VALUES(null,?,?,?,?,?,?,null,?);`,
+      [propertyId, creatorId, created, request.title, request.description, request.attachedFiles, 1]).then( () => {
+      return database.close();
+    });
+  },
+
+  deleteRequest: function(id) {
+    database.open();
+    return database.query(`DELETE FROM maint_request WHERE id = ?;`,
+                          [id]).then(() => {
+      //Do I need to return results here?  Or does promise cover failure case
+      return database.close();
+    });
+  },
+
+
+  convertIntToStatus(request) {
+    switch(request.status) {
+      case 1:
+        return 'open'
+        break;
+      case 2:
+        return 'pending'
+        break;
+      case 3:
+        return 'closed'
+        break;
+      case 4:
+        return 'confirmed'
+        break;
+      default:
+        return 'open'
+        break;
+    }
+  }
+  convertStatusToInt(request) {
+    switch(request.status) {
+      case 'open':
+        return 1
+        break;
+      case 'pending':
+        return 2
+        break;
+      case 'closed':
+        return 3
+        break;
+      case 'confirmed':
+        return 4
+        break;
+      default:
+        return 'open'
+        break;
+    }
+  }
+  //This lets us mock out the database, so that we can check calls, supply responses
+  setDatabase: function(newDb) {
+    database = newDb
+  }
+}
